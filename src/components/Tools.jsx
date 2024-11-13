147d25297107b24e700de69a7a3eec85e8418918
drummond12 <drummond12@googlemail.com>
2024-11-13 22:33:18 +0000

    cleanup

---
+++ src/components/Tools.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import logo from '../assets/cat.jpg';
+
+export default function Tools() {
+    return (
+        <div className='tools'>
+          <img src={logo} className="logo" alt="Cat logo" />
+          <h1>Under construction page 3</h1>
+        </div>
+      );
+}
